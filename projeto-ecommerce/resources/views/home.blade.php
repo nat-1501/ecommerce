f87ce5d2768bbd5423f2cbed21a40cb9063e99f5
nat-1501 <natalisoares90@gmail.com>
2021-12-30 11:39:45 -0300

    Rota home laravel para aplicação

---
+++ projeto-ecommerce/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NatiShoes - Ecommerce</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/4.5.2/css/bootstrap.min.css"
+    <link rel="stylesheet" 
+</head>
+<body>
+    
+</body>
+</html>
