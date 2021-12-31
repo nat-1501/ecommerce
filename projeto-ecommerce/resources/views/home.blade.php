7271ec57b80bfd944084f51fcacc91e86062f2c9
nat-1501 <natalisoares90@gmail.com>
2021-12-31 14:30:36 -0300

    feita a navegação do nav bar

--- projeto-ecommerce/resources/views/home.blade.php
+++ projeto-ecommerce/resources/views/home.blade.php
@@ -5,10 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NatiShoes - Ecommerce</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/4.5.2/css/bootstrap.min.css"
-    <link rel="stylesheet" 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+        <a href="#" class="navbar-brand">NatiShoes</a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Categorias</a>
+                <a class="nav-link" href="#">Cadastrar</a>
+                <a class="nav-link" href="#">Pedidos</a>
+
+
+            </div>
+
+        </div>
+    </nav>
 </body>
 </html>
