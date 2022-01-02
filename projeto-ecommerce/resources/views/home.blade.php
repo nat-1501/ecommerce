97e512daf7d8de1fbcae874a81bc7572e8a01d19
nat-1501 <natalisoares90@gmail.com>
2022-01-01 23:10:43 -0300

    estilizando com bootstrap

--- projeto-ecommerce/resources/views/home.blade.php
+++ projeto-ecommerce/resources/views/home.blade.php
@@ -19,12 +19,22 @@
                 <a class="nav-link" href="#">Pedidos</a>
             </div>
         </div>
+
+        <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Busque aqui</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="" aria-label="Buscar">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
         <a href="#" class="btn btn-sm"></a>
        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
-   
- 
+
+        
     </nav>
 
 
@@ -35,7 +45,7 @@
                     <img src="{{ asset('imagens/01.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Mocassim</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" type="button" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
 
                 </div>
@@ -45,8 +55,8 @@
                 <div class="card">
                     <img src="{{ asset('imagens/02.jpg') }}" class="card-img-top"/>
                     <div class="card-body">
-                        <h6 class="card-title">Sapato Frente aberta Preto</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <h6 class="card-title">Sapato Frente aberta</h6>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
             </div>   
@@ -56,7 +66,7 @@
                     <img src="{{ asset('imagens/3.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Bocassim</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -67,7 +77,7 @@
                     <img src="{{ asset('imagens/04.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Salto Baixo Fino Raro</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -78,7 +88,7 @@
                     <img src="{{ asset('imagens/05.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Salto Eférna</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -89,7 +99,7 @@
                     <img src="{{ asset('imagens/6.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Tenis Feminino Branco</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -100,7 +110,7 @@
                     <img src="{{ asset('imagens/08.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Rasteira Abacaxi</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -111,7 +121,7 @@
                     <img src="{{ asset('imagens/10.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Fivela</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -122,7 +132,7 @@
                     <img src="{{ asset('imagens/13.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Salto Alto Preto</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -133,7 +143,7 @@
                     <img src="{{ asset('imagens/15.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Baixo Branco</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -144,7 +154,7 @@
                     <img src="{{ asset('imagens/14.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Frente aberta Bege</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -155,7 +165,7 @@
                     <img src="{{ asset('imagens/12.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Alto Preto</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -166,7 +176,7 @@
                     <img src="{{ asset('imagens/16.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Tenis Amarração Branco</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -176,7 +186,7 @@
                     <img src="{{ asset('imagens/17.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sandalia Amarração Amarela </h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -187,7 +197,7 @@
                     <img src="{{ asset('imagens/20.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Alto Bloco Marrom </h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -198,7 +208,7 @@
                     <img src="{{ asset('imagens/21.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Alto Pata Aberta </h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -208,7 +218,7 @@
                     <img src="{{ asset('imagens/22.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Sapato Salto Alto Bege</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -218,7 +228,7 @@
                     <img src="{{ asset('imagens/23.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Salto Fino Preto</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -229,7 +239,7 @@
                     <img src="{{ asset('imagens/24.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Bota Tratorada </h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -240,7 +250,7 @@
                     <img src="{{ asset('imagens/25.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Bota Calcanhar</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -250,7 +260,7 @@
                     <img src="{{ asset('imagens/26.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Bota Neon</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
@@ -260,7 +270,7 @@
                     <img src="{{ asset('imagens/27.jpg') }}" class="card-img-top" />
                     <div class="card-body">
                         <h6 class="card-title">Bota Fivela </h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item ao carrinho</a>
+                        <a href="#" class="btn btn-outline-dark">Adicionar item</a>
                     </div>
                 </div>
                  
