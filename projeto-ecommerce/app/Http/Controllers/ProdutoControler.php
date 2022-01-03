0c4b0abc81c5f57832b38e2eefa85994c597436c
nat-1501 <natalisoares90@gmail.com>
2022-01-03 20:05:34 -0300

    Configurando as rotas

---
+++ projeto-ecommerce/app/Http/Controllers/ProdutoControler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControler extends Controller
+{
+    public function index(Request $request){
+        $data = [];
+
+        return view("home", $data);
+    }
+
+    public function categoria(Request $request){
+        $data=[];
+
+        return view("categoria", $data);
+
+    }
+}
--- projeto-ecommerce/routes/web.php
+++ projeto-ecommerce/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\ProdutoController;
+use App\Http\Controller\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::match(['get','post'], '/', [ ProdutoController::class, 'index']) 
+    ->name('home');
+Route::match(['get','post'], '/categoria', [ ProdutoController::class, 'categoria'])
+    ->name('categoria');
+Route::match(['get','post'], '/cadastrar', [ ClienteController::class, 'cadastrar'])       
+    ->name('cadastrar');
